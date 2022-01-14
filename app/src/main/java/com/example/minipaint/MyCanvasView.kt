48f85730b722cceae59564eedba0387a4019e88f
Jøran Haines <joran.haines@gmail.com>
2022-01-14 13:39:05 +0530

    Implements touch events

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 
 private const val STROKE_WIDTH = 12f
@@ -18,10 +19,14 @@ class MyCanvasView(context: Context) : View(context) {
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
     private var path = Path()
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
 
-    private val painter = Paint().apply {
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val paint = Paint().apply {
         color = drawColor
         isAntiAlias = true
         isDither = true
@@ -54,15 +59,29 @@ class MyCanvasView(context: Context) : View(context) {
     }
 
     private fun touchUp() {
-        TODO("Not yet implemented")
+        path.reset()
     }
 
     private fun touchMove() {
-        TODO("Not yet implemented")
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier(draws curves, not just straight lines) from the last point,
+                // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
     }
 
     private fun touchStart() {
-        TODO("Not yet implemented")
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
     }
 
     override fun onDraw(canvas: Canvas?) {
